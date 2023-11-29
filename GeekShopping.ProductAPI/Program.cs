16cbc9829ce16ab397305f5d2e3f567613c273dc
gui-laranjeira <guilr14@gmail.com>
2023-11-28 23:01:34 -0300

    feat: add model and migration to product api

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace GeekShopping.ProductAPI
 {
     public class Program
@@ -12,6 +15,13 @@ namespace GeekShopping.ProductAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<MySQLContext>(options =>
+                options.UseMySql(
+                    builder.Configuration.GetConnectionString("MySQLConnection"),
+                    new MySqlServerVersion(new Version(8, 2, 0))
+                )
+            );
 
             var app = builder.Build();
 
