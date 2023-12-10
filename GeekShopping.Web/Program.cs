57896b1c9ce95f7f97a19aff3624e0c58e7eb375
gui-laranjeira <guilr14@gmail.com>
2023-12-10 15:01:45 -0300

    feat: add authentication configs to web project

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,5 +1,6 @@
 using GeekShopping.Web.Services;
 using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
 namespace GeekShopping.Web
 {
@@ -12,10 +13,33 @@ namespace GeekShopping.Web
             // Add services to the container.
             
             builder.Services.AddControllersWithViews();
-
+      
             builder.Services.AddHttpClient<IProductService, ProductService>( c =>
                     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
                 );
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookie";
+                options.DefaultChallengeScheme = "oidc";
+            })
+              .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+              .AddOpenIdConnect("oidc", options =>
+              {
+                  options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+                  options.GetClaimsFromUserInfoEndpoint = true;
+                  options.ClientId = "geek_shopping";
+                  options.ClientSecret = "my_super_secret";
+                  options.ResponseType = "code";
+                  options.ClaimActions.MapJsonKey("role", "role", "role");
+                  options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+                  options.TokenValidationParameters.NameClaimType = "name";
+                  options.TokenValidationParameters.RoleClaimType = "role";
+                  options.Scope.Add("geek_shooping");
+                  options.SaveTokens = true;
+              });
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -27,7 +51,7 @@ namespace GeekShopping.Web
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
